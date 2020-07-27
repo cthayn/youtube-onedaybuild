53898c8df41d1005bb0a5f2c4dc2c64464cd2b98
Corey Thayn <corey_thayn@hotmail.com>
2020-07-27 15:28:54 -0600

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Corey Thayn on 7/27/20.
+//  Copyright © 2020 Corey Thayn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
